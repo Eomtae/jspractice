58ad59456229e6a75f397c1a6796a56804cd8690
Eomtae <theum0723@gmail.com>
2023-05-08 11:32:53 +0900

    const-let

---
+++ app.js
@@ -0,0 +1,12 @@
+const math = 6
+const b = 2;
+
+let myName = 'eom';
+
+console.log(math + b)
+console.log(math * b)
+console.log(math / b)
+console.log('hello '+myName)
+
+myName="taehyeon";
+console.log('your new name is ' + myName)
